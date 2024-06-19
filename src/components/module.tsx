3176e43431747924df06f9759e6f205a2f1722db
Lucas Ribeiro <lucasribeirodesouza@live.com>
2024-06-19 16:34:07 -0300

    feat: change video on select

--- src/components/module.tsx
+++ src/components/module.tsx
@@ -2,6 +2,8 @@ import * as Collapsible from '@radix-ui/react-collapsible'
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./lesson";
 import { useAppSelector } from '../store';
+import { useDispatch } from 'react-redux';
+import { play } from '../store/slices/player';
 
 interface ModuleProps {
   moduleIndex: number;
@@ -10,6 +12,8 @@ interface ModuleProps {
 }
 
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
+  const dispatch = useDispatch()
+
   const lessons = useAppSelector((state) => {
     return state.player.course.modules[moduleIndex].lessons
   })
@@ -31,13 +35,13 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-        {
-          lessons.map((lesson) => {
-            return (
-              <Lesson key={lesson.id} title={lesson.title} duration={lesson.duration} />
-            )
-          })
-        }
+          {
+            lessons.map((lesson, lessonIndex) => {
+              return (
+                <Lesson key={lesson.id} title={lesson.title} duration={lesson.duration} onPlay={() => dispatch(play([moduleIndex, lessonIndex]))} />
+              )
+            })
+          }
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
