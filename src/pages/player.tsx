6b44c254178f09cbdd41654551271647d9279e54
Lucas Ribeiro <lucasribeirodesouza@live.com>
2024-06-19 23:08:41 -0300

    feat: create load course async thunk

--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -3,19 +3,27 @@ import { MessageCircle } from "lucide-react";
 import { Header } from "../components/header";
 import { Video } from "../components/video";
 import { Module } from "../components/module";
-import { useAppSelector } from "../store";
-import { useCurrentLesson } from "../store/slices/player";
+import { useAppDispatch, useAppSelector } from "../store";
+import { loadCourse, useCurrentLesson } from "../store/slices/player";
 import { useEffect } from "react";
 
 export function Player() {
+  const dispatch = useAppDispatch()
+
   const modules = useAppSelector(state => {
-    return state.player.course.modules
+    return state.player.course?.modules
   })
 
   const { currentLesson } = useCurrentLesson()
 
   useEffect(() => {
-    document.title = `Player | ${currentLesson.title}`
+    dispatch(loadCourse())
+  }, [])
+
+  useEffect(() => {
+    if (currentLesson) {
+      document.title = `Player | ${currentLesson.title}`
+    }
   }, [currentLesson])
 
   return (
@@ -36,7 +44,7 @@ export function Player() {
           </div>
           <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
             {
-              modules.map((module, index) => {
+              modules && modules.map((module, index) => {
                 return (
                   <Module key={module.id} moduleIndex={index} title={module.title} amountOfLessons={module.lessons.length} />
                 )
